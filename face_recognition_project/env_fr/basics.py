2e683c3f8b9341c7bf480d8a1ccdd5bb55f392d1
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-09 16:26:52 -0400

    fix syntx mistakes

--- face_recognition_project/env_fr/basics.py
+++ face_recognition_project/env_fr/basics.py
@@ -3,9 +3,9 @@ import numpy as np
 import face_recognition
 
 imgTrump = face_recognition.load_image_file('ImageBasic/45_donald_trump_w-1250.jpg')
-imgTrump = cv2.cvtColor(imgTrump.cv2.COLOR_BGR2RRGB)
+imgTrump = cv2.cvtColor(imgTrump,cv2.COLOR_BGR2RGB)
 imgTest = face_recognition.load_image_file('ImageBasic/trump.jpg')
-imgTest = cv2.cvtColor(imgTrump.cv2.COLOR_BGR2RRGB)
+imgTest = cv2.cvtColor(imgTrump,cv2.COLOR_BGR2RGB)
 
 faceLoc = face_recognition.face_loations(imgTrump)[0]
 encodeTrump = face_recognition.face_encodings(imgTrump)[0]
