407b886d8a13bd85492b0ab07fe05c43f82cd4c9
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-09 16:24:29 -0400

    upload images and created implementation to load, convert image to RGB, and return measurements of facial features

--- face_recognition_project/env_fr/basics.py
+++ face_recognition_project/env_fr/basics.py
@@ -1,2 +1,16 @@
 import cv2
-import numpy as n
+import numpy as np
+import face_recognition
+
+imgTrump = face_recognition.load_image_file('ImageBasic/45_donald_trump_w-1250.jpg')
+imgTrump = cv2.cvtColor(imgTrump.cv2.COLOR_BGR2RRGB)
+imgTest = face_recognition.load_image_file('ImageBasic/trump.jpg')
+imgTest = cv2.cvtColor(imgTrump.cv2.COLOR_BGR2RRGB)
+
+faceLoc = face_recognition.face_loations(imgTrump)[0]
+encodeTrump = face_recognition.face_encodings(imgTrump)[0]
+cv2.rectangle(imgTrump,(faceLoc[3],faceLoc[0]),(faceLoc[1],faceLoc[2]),(255, 0, 255),2)
+
+cv2.imshow('Donald Trump', imgTrump)
+cv2.imshow('Donald Test', imgTest)
+cv2.waitKey(0)
