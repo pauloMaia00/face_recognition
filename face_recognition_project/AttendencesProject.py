8787257138fe541d789e974de801f00a8cbdfca8
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-16 15:11:51 -0400

    change the implementation to just display the names

--- face_recognition_project/AttendencesProject.py
+++ face_recognition_project/AttendencesProject.py
@@ -9,10 +9,12 @@ images = []
 classNames = []
 myList = os.listdir(path)
 print(myList)
+# uses the names and import the images one by one
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    #removes the file type and just displays the names
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
 
-imgTrump = face_recognition.load_image_file('ImageBasic/45_donald_trump_w-1250.jpg')
-imgTrump = cv2.cvtColor(imgTrump,cv2.COLOR_BGR2RGB)
-imgTest = face_recognition.load_image_file('ImageBasic/trump.jpg')
-#imgTest = face_recognition.load_image_file('ImageBasic/bill_clinton.jpg')
-imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
-
+#
