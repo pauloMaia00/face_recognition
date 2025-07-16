7accaf7c16e95f5171829ec7bd2e877dc46843c8
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-16 14:54:09 -0400

    testing

--- face_recognition_project/basics.py
+++ face_recognition_project/basics.py
@@ -4,8 +4,8 @@ import face_recognition
 
 imgTrump = face_recognition.load_image_file('ImageBasic/45_donald_trump_w-1250.jpg')
 imgTrump = cv2.cvtColor(imgTrump,cv2.COLOR_BGR2RGB)
-# imgTest = face_recognition.load_image_file('ImageBasic/trump.jpg')
-imgTest = face_recognition.load_image_file('ImageBasic/bill_clinton.jpg')
+imgTest = face_recognition.load_image_file('ImageBasic/trump.jpg')
+#imgTest = face_recognition.load_image_file('ImageBasic/bill_clinton.jpg')
 imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
 
 faceLoc = face_recognition.face_locations(imgTrump)[0]
