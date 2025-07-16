d32338e2fa9287f8bb482a02f17e365b18a57ab9
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-16 15:35:57 -0400

    fixed variable mismatch

--- face_recognition_project/AttendencesProject.py
+++ face_recognition_project/AttendencesProject.py
@@ -38,7 +38,7 @@ cap = cv2.VideoCapture(0)
 while True:
         success, img = cap.read()
         imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-        imgs = cv2.cvtColor(imgs,cv2.COLOR_BGR2RGB)
+        imgS = cv2.cvtColor(imgS,cv2.COLOR_BGR2RGB)
         #encoding of our webcam
         facesCurFrame = face_recognition.face_locations(imgS)
         encodesCurFrame= face_recognition.face_encodings(imgs, facesCurFrame)
