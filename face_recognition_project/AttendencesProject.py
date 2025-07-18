2c99899e62adbc71981c3f81585441a3f4c2c546
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-18 11:31:46 -0400

    update program to show the attendence from the rest of the group

--- face_recognition_project/AttendencesProject.py
+++ face_recognition_project/AttendencesProject.py
@@ -42,7 +42,6 @@ def mark_attendence(name):
             f.writelines(f'\n{name}, {dtString}')
             
 
-mark_attendence('trump')
 encodeListKnown = find_encodings(images)
 print("Encoding Complete!")
 
@@ -66,14 +65,15 @@ while True:
             matchIndex = np.argmin(faceDis) # with this we should now know which person we are talking about
 
             if matches[matchIndex]:
-                 name = classNames[matchIndex].upper()
-                 #print(name)
-                 y1,x2,y2,x1 = faceLoc
-                 y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
-                 cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                 cv2.rectangle(img, (x1,y2-35),(x2, y2), (0, 255, 0), cv2.FILLED)
-                 cv2.putText(img, name, (x1+6, y1-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
-
+                name = classNames[matchIndex].upper()
+                #print(name)
+                y1,x2,y2,x1 = faceLoc
+                y1,x2,y2,x1 = y1*4,x2*4,y2*4,x1*4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1,y2-35),(x2, y2), (0, 255, 0), cv2.FILLED)
+                cv2.putText(img, name, (x1+6, y1-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
+                 
+                mark_attendence(name)
         cv2.imshow('Webcam', img)
         cv2.waitKey(1)
 
