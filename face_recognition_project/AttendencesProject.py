6cf0aa6d283ad9b1b307e79a06b441da5107bbca
Paulo Maia <pjm2098@email.vccs.edu>
2025-07-18 11:04:14 -0400

    displaying name of person associated with the img shown on th webcam

--- face_recognition_project/AttendencesProject.py
+++ face_recognition_project/AttendencesProject.py
@@ -53,10 +53,14 @@ while True:
             if matches[matchIndex]:
                  name = classNames[matchIndex].upper()
                  print(name)
+                 y1,x2,y2,x1 = faceLoc
+                 cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                 cv2.rectangle(img, (x1,y2-35),(x2, y2), (0, 255, 0), cv2.FILLED)
+                 cv2.putText(img, name, (x1+6, y1-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255,255,255), 2)
 
         cv2.imshow('Webcam', img)
         cv2.waitKey(1)
-        
+
 
 
 # faceLoc = face_recognition.face_locations(imgTrump)[0]
